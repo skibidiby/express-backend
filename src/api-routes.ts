576792336665f52012c40ed81b67a93fd1c144b7
skibidiby <skibidiby@outlook.com>
2021-03-31 19:38:09 +0300

    🚧 routes added

---
+++ src/api-routes.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from "express";
+export let Router = express.Router();
+
+Router.get("/", (req: Request, res: Response) => {
+  res.json({
+    status: "API works",
+    message: "Boilerprate prty cool, right?",
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
-import {json} from "body-parser";
+import { json } from "body-parser";
+import { Router } from "./api-routes";
 let app = express();
 var port = 3000;
 
@@ -8,4 +9,7 @@ app.listen(port, () => {
   console.log("server is listening on port", port);
 });
 
-app.get("/", (req:Request, res:Response) => res.send("Hello World with Express"));
+app.get("/", (req: Request, res: Response) =>
+  res.send("Hello World with Express")
+);
+app.use("/api", Router);
